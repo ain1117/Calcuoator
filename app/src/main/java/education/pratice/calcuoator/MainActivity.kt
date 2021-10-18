b9a38acec3458cf13ea3341f57dc5abfb46fcb28
AinJo <ainwithcat@gmail.com>
2021-10-18 14:00:21 +0900

    Initial commit

---
+++ app/src/main/java/education/pratice/calcuoator/MainActivity.kt
@@ -0,0 +1,28 @@
+package education.pratice.calcuoator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClicked(v: View) {
+
+    }
+
+    fun clearButtonClicked(view: android.view.View) {
+
+    }
+    fun resultClicked(view: android.view.View) {
+
+    }
+    fun historybuttonClicked(view: android.view.View) {
+
+    }
+
+    fun resultbuttonClicked(view: android.view.View) {}
+}
